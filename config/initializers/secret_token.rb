56c50628fcdc2931fbe5257f7b7680448bb2113a
sb4f22 <sb4f22@sb4f22-HP-Compaq-dc7600-Convertible-Minitower.(none)>
2012-10-21 19:01:51 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpApp::Application.config.secret_token = 'bf353bf3c310070d2a9c8c4582e1bf18eb40ff87e63281a831b5c12a7ae2629c00412b4ca7f7eecedc9d3a6e9fcdd4b149ee92bb3f2b3af4dd28056e79a10928'
